d372699d872672040eade485c2a2963b5272811e
Trixter__JDM <54733786+trixtertop@users.noreply.github.com>
2023-04-10 14:41:00 +0600

    start busines logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,24 @@ let symm = 3000;
 
 const price = document.querySelector(".generalSym__price");
 
-const querySelector = (select, num) => {
-  const selector = document.querySelector(select);
-  selector.addEventListener("click", (e) => {
+// const querySelector = (select, num) => {
+//   const selector = document.querySelector(select);
+//   selector.addEventListener("click", (e) => {
+//     if (e.isTrusted) {
+//       const generalSymm = (symm += +num);
+//       console.log(generalSymm);
+//       price.innerHTML = generalSymm;
+//     }
+//   });
+// };
+
+// querySelector(".input_check", 100);
+
+const xz = document.querySelectorAll(".input_check");
+
+const querySelector = (select, num, arr) => {
+  const selector = document.querySelectorAll(select);
+  selector[arr].addEventListener("click", (e) => {
     if (e.isTrusted) {
       const generalSymm = (symm += +num);
       console.log(generalSymm);
@@ -13,4 +28,6 @@ const querySelector = (select, num) => {
   });
 };
 
-querySelector(".input_check", 100);
+querySelector(".input_check", 200, 0);
+
+querySelector(".input_check", 500, 1);
