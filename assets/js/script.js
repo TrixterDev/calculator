274f00623e580681833a45c4356cc0bb0faed07e
Trixter__JDM <54733786+trixtertop@users.noreply.github.com>
2023-04-11 11:09:03 +0600

    create drop-down list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,39 @@
-let symm = 3000;
+// let symm = 3000;
 
-const price = document.querySelector(".generalSym__price");
+// const price = document.querySelector(".generalSym__price");
 
-// const querySelector = (select, num) => {
-//   const selector = document.querySelector(select);
-//   selector.addEventListener("click", (e) => {
+// // const querySelector = (select, num) => {
+// //   const selector = document.querySelector(select);
+// //   selector.addEventListener("click", (e) => {
+// //     if (e.isTrusted) {
+// //       const generalSymm = (symm += +num);
+// //       console.log(generalSymm);
+// //       price.innerHTML = generalSymm;
+// //     }
+// //   });
+// // };
+
+// // querySelector(".input_check", 100);
+
+// const xz = document.querySelectorAll(".input_check");
+
+// const querySelector = (select, num, arr, rate) => {
+//   const selector = document.querySelectorAll(select);
+//   selector[arr].addEventListener("click", (e) => {
 //     if (e.isTrusted) {
-//       const generalSymm = (symm += +num);
-//       console.log(generalSymm);
-//       price.innerHTML = generalSymm;
+//       let generalSymm = (symm += +num);
+//       price.innerHTML = generalSymm += +rate;
+//       arr === 55 && 54 && 53 && 52 ? (price.innerHTML = "Договорная") : null;
+//       console.log(e);
 //     }
 //   });
 // };
 
-// querySelector(".input_check", 100);
-
-const xz = document.querySelectorAll(".input_check");
-
-const querySelector = (select, num, arr, rate) => {
-  const selector = document.querySelectorAll(select);
-  selector[arr].addEventListener("click", (e) => {
-    if (e.isTrusted) {
-      let generalSymm = (symm += +num);
-      price.innerHTML = generalSymm += +rate;
-    }
-  });
-};
-
-querySelector(".input_check", 1000, 54, 18000);
+// querySelector(".input_check", 500, 55, 18000);
+// querySelector(".input_check", 500, 54, 18000);
+// querySelector(".input_check", 500, 53, 18000);
+// querySelector(".input_check", 500, 52, 18000);
+// querySelector(".input_check", 1000, 51, 18000);
+// querySelector(".input_check", 500, 50, 18000);
+// querySelector(".input_check", 0, 49, 18000);
+// querySelector(".input_check", 0, 48, 18000);
